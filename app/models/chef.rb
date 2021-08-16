91564bfb9bda9c124cc86162ab325b2270e22e48
Igor_Medeiros <igormedeiros1999@gmail.com>
2021-08-16 11:30:55 -0300

    update validates, case sensitive and valid regex

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,4 +1,8 @@
 class Chef <ApplicationRecord
-    validates :name, presence :true
-    validates :email, presence :true
+    validates :name, presence :true, length: {maximum: 30}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.][a-z]+\z/j
+    validates :email, presence :true, length: {maximum: 255}, 
+                        format: {with: VALID_EMAIL_REGEX},
+                        uniqueness: {case_sensitive: false}
+
 end
