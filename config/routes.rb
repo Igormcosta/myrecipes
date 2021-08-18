c36d46340e3c116ecf988e142c347cb9b396e460
Igor_Medeiros <igormedeiros1999@gmail.com>
2021-08-18 14:30:50 -0300

    add new

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  #hieraquia de route
   root "pages#home"
   get '/home', to: 'pages#home'
 
   get '/recipes', to: 'recipes#index'
-  get '/recipes/:id', to:'recipes#show', as: 'recipe'
+  get '/recipes/new', ro: 'recipes#new', as: 'new_recipe'
+  get '/recipes/:id', to: 'recipes#show', as: 'recipe'
+  
 end
