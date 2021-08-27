3d4409b42d3db821519924bc952a67cde198fcd3
Igor_Medeiros <igormedeiros1999@gmail.com>
2021-08-27 07:53:37 -0300

    add admin and admin permissions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -54,9 +54,9 @@ class RecipesController < ApplicationController
         end
 
         def require_some_user
-            if current_chef != @recipe.chef
-                flash[:danger] = "you can only edit or delete your own recipes"
+            if current_chef != @recipe.chef and !current_chef.admin?
+                flash[:danger] = "You can only edit or delete your own recipes"
                 redirect_to recipes_path
-            end
+            end  
         end
 end
