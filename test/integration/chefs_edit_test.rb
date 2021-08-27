e9462424037ffe3366ea71561490aab10ab5fdea
Igor_Medeiros <igormedeiros1999@gmail.com>
2021-08-27 10:32:01 -0300

    complete admin

--- test/integration/chefs_edit_test.rb
+++ test/integration/chefs_edit_test.rb
@@ -4,6 +4,8 @@ class ChefsEditTest < ActionDispatch::IntegrationTest
   
   def setup
     @chef = Chef.create!(chefname: "Igor", email: "igor@example.com", password: "password", password_confirmation: "password")
+    @chef2 = Chef.create!(chefname: "john", email: "john@example.com", password: "password", password_confirmation: "password")
+    @admin_user = Chef.create! (chefname: "zezin", email: "zezin@example.com", password: "password", password_confirmation: "password", admin: true)
   end
 
   test "reject an invalid edit" do
@@ -16,11 +18,11 @@ class ChefsEditTest < ActionDispatch::IntegrationTest
     assert_select 'div.panel-body'
   end
 
-  test "accept valid signup" do
+  test "accept valid edit" do
     sign_in_as(@chef, "password")
     get edit_chef_path(@chef)
     assert_template 'chefs/edit'
-    patch chef_path(@chef), params: { chef: {chefname: "igor1" , email: "igor1@example.com", password: "password", password_confirmation: " "}}
+    patch chef_path(@chef), params: { chef: {chefname: "igor1" , email: "igor1@example.com"}}
     assert_redirected_to @chef
     assert_not flash.empty?
     @chef.reload
@@ -28,4 +30,28 @@ class ChefsEditTest < ActionDispatch::IntegrationTest
     assert_match "igor1@example.com", @chef.email
   end
 
+  test "accept edit attempt by admin user" do
+    sign_in_as(@admin_user, "password")
+    get edit_chef_path(@chef)
+    assert_template 'chefs/edit'
+    patch chef_path(@chef), params: { chef: {chefname: "igor3" , email: "igor3@example.com"}}
+    assert_redirected_to @chef
+    assert_not flash.empty?
+    @chef.reload
+    assert_match "igor3", @chef.chefname
+    assert_match "igor3@example.com", @chef.email
+  end
+
+  test "redirect edit attempt by another non-admin user" do
+    sign_in_as(@chef2, "password")
+    updated_name = "joe"
+    updated_email = "joe@example.com"
+    patch chef_path(@chef), params: { chef: {chefname: updated_name , email: updated_email}}
+    assert_redirected_to @chef
+    assert_not flash.empty?
+    @chef.reload
+    assert_match "igor1", @chef.chefname
+    assert_match "igor1@example.com", @chef.email
+  end
+  
 end
