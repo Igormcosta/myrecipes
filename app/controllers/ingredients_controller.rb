bdeab5301efe6479aafb207cbc24f15b10d2ed75
Igor_Medeiros <igormedeiros1999@gmail.com>
2021-08-31 09:12:55 -0300

    add ingredient show

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,4 +1,5 @@
 class IngredientsController < ApplicationController
+    before_action :set_ingredient, only: [:edit, :update, :show]
 
     def new
 
@@ -13,11 +14,16 @@ class IngredientsController < ApplicationController
     end
 
     def show 
-
+        @ingredient_recipes = @ingredient.recipes.paginate(page: params[:page], per_page: 5)
     end
 
     def index
         @ingredients = Ingredient.paginate(page: params[:page], per_page: 5)
     end
 
+    private 
+
+    def set_ingredient
+        @ingredient = Ingredient.find(params[:id])
+    end
 end
